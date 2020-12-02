fbecd4182649bc396fd4e56ca06f91a22e8675a9
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2020-12-01 23:24:13 -0800

    Add files via upload

---
+++ matrixElementsSum.java
@@ -0,0 +1,15 @@
+
+public class matrixElementsSum {
+	public int matrixElemSum(int[][] matrix) {
+	    int sum = 0;
+	    for(int i = 0; i < matrix[0].length; i++){
+	        for(int j = 0; j < matrix.length; j++){
+	            if(matrix[j][i] > 0){
+	                sum += matrix[j][i];
+	            }
+	            else break;
+	        }
+	    }
+	    return sum;
+	}
+}

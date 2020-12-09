2341bd03f456989574f3233af09a5de3253ffc66
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2020-12-09 00:37:33 -0800

    done

--- alterSum.java
+++ alterSum.java
@@ -4,6 +4,22 @@ public class alterSum {
 		int[] ans = new int[2];
 		int firstSum = 0;
 		int secondSum = 0;
+		firstSum += a[0];
+		if(a.length > 1) {
+			for(int i = 1; i < a.length; i++ ) {
+				if(i%2 != 0) {//odds
+					secondSum += a[i];
+				}
+				else if(i%2 == 0) {//even
+					firstSum += a[i];
+				}
+				
+			}
+		}
+		
+		ans[0]=firstSum;
+		ans[1]=secondSum;
+
 		return ans;
 	}
 	

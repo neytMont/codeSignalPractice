e68766d4425ba6714c14de8cce02a8834cfa7e7d
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2020-12-11 00:41:03 -0800

    Update border.java

--- border.java
+++ border.java
@@ -1,4 +1,15 @@
 
 public class border {
-
+	public String[] addBorder(String[] picture) {
+		String border = "*";
+		String[] ans = new String[picture.length+2];
+		String sentence = "";
+		
+		int strLength = picture[0].length();
+		
+		for(int i  = 0; i < strLength; i++) {
+			
+		}
+		
+	}
 }

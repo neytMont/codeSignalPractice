ecb54c18ab41bb5b9af3eba30e6f84d69f311ac4
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2020-12-12 23:15:38 -0800

    Update border.java

--- border.java
+++ border.java
@@ -1,15 +1,29 @@
-
 public class border {
 	public String[] addBorder(String[] picture) {
-		String border = "*";
+		StringBuilder str = new StringBuilder();
+		StringBuilder str1;
+	
 		String[] ans = new String[picture.length+2];
-		String sentence = "";
 		
 		int strLength = picture[0].length();
 		
-		for(int i  = 0; i < strLength; i++) {
-			
+		for(int i  = 0; i < strLength+2; i++) {//how much asterisk the top & bottom border
+			str.append("*");
 		}
 		
+		ans[0] = str.toString();//top border
+			
+		for(int i = 0; i< picture.length; i++) {//putting side borders
+			str1 = new StringBuilder();
+			str1.append("*" + picture[i] + "*");
+			ans[i+1] = str1.toString();
+		}
+		
+		ans[ans.length-1] = str.toString(); //bottom border
+		
+		for(int i = 0; i < ans.length; i++) {//printing
+			System.out.println(ans[i]);
+		}
+		return ans;
 	}
 }

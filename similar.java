c6ac5a9b6eeeaae74dbdba0ea06ca83579dc04b8
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2020-12-18 18:17:53 -0800

    Update similar.java

--- similar.java
+++ similar.java
@@ -1,8 +1,8 @@
 
 public class similar {
 	public boolean areSimilar(int[] a, int[] b) {
-		//Sort both a and b
-		//then compare
+		//Sort both a and b. then compare
+		//loop array
 		for(int i = 0; i < a.length; i++) {
 			
 		}

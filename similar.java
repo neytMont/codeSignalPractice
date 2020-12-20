640f36ab110eede88ba3b3b423f64abe2c722ac7
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2020-12-19 23:41:42 -0800

    Update similar.java

--- similar.java
+++ similar.java
@@ -1,9 +1,31 @@
 
 public class similar {
 	public boolean areSimilar(int[] a, int[] b) {
-		//Sort both a and b. then compare
-		for(int i = 0; i < a.length; i++) {
-			
+				ArrayList<Integer> bList = new ArrayList<>();
+		for(int i = 0; i < b.length; i++) {
+			bList.add(b[i]);
 		}
+		
+		int count = 0;
+		while(count <= 1) {
+			for(int i = 0; i < a.length; i++) {
+					
+				if(a[i] != bList.get(i)) {
+					count++;
+					
+					int x = bList.indexOf(a[i]);//the right num
+					int curr = bList.indexOf(b[i]);//current index
+					if(bList.contains(a[i])) {
+						Collections.swap(bList, x, curr);
+					}
+					
+				}
+			}
+			if(count > 1) {
+				break;
+			}
+			return true;
+		}
+		return false;
 	}
 }

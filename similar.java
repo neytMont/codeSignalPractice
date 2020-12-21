d0e44a51846f011116054ad7daa300660a03d51f
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2020-12-21 14:47:53 -0800

    Update similar.java

--- similar.java
+++ similar.java
@@ -1,26 +1,35 @@
 public class similar {
 	public boolean areSimilar(int[] a, int[] b) {
-		ArrayList<Integer> bList = new ArrayList<>();
-		for(int i = 0; i < b.length; i++) {
-			bList.add(b[i]);
-		}
-		int count = 0;
-		while(count <= 1) {
-			for(int i = 0; i < a.length; i++) {
-				if(a[i] != bList.get(i)) {
-					count++;
-					int x = bList.indexOf(a[i]);//the right num
-					int curr = bList.indexOf(b[i]);//current index
-					if(bList.contains(a[i])) {
-						Collections.swap(bList, x, curr);
-					}
-				}
+	    if(Arrays.equals(a, b)){
+		return true;
+	    }
+	    ArrayList<Integer> bList = new ArrayList<>();//make the second array a list
+	    for(int i = 0; i < b.length; i++) {//add elements of b to arraylist
+		bList.add(b[i]);
+	    }
+	    int count = 2;//how many swaps
+	    while(count < 1) {//
+		for(int i = 0; i < a.length; i++) {
+
+		    if(a[i] != bList.get(i)) {//if the element on a is not equal to element on b
+			count--;//subtract 1 to count
+
+			int x = bList.indexOf(a[i]);//index of right element
+			int curr = bList.indexOf(b[i]);//current index thats wrong
+			System.out.println(x + " "+ a[i] +"\n" + curr + " " + b[i]);
+			if(bList.contains(a[i])) {//look for the element
+			    Collections.swap(bList, x, curr);//swap
+			    continue;
 			}
-			if(count > 1) {
-				break;
-			}
-			return true;
+
+		    }
+		    count--;
 		}
-		return false;
+		if(count > 1) {
+		    break;
+		}
+		return true;
+	    }
+	    return false;
 	}
 }

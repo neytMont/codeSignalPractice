3fbb27e575bbae9d66237c47d0eb8d25b410b2d2
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2020-12-23 16:54:45 -0800

    Update arrChange.java

--- arrChange.java
+++ arrChange.java
@@ -1,13 +1,12 @@
-
 public class arrChange {
 	public int arrayChange(int[] inputArray) {
-		int count = 0;
-		for(int i = 0; i < inputArray.length - 1; i++) {
-			while(inputArray[i+1] <= inputArray[i]) {
-				inputArray[i+1]++;
-				count++;
+		int count = 0;//count variable
+		for(int i = 0; i < inputArray.length - 1; i++) {//loop through the array except the last element
+			while(inputArray[i+1] <= inputArray[i]) {//while the next element is less than or equal to the current element
+				inputArray[i+1]++;//add 1 to the next element 
+				count++;//add the count
 			}
 		}
-		return count;
+		return count;//return the count
 	}
 }

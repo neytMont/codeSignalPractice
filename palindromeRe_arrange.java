c56e96d694fb32418be48305a86d200a3bae1bc9
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2020-12-26 17:41:57 -0800

    Add files via upload

---
+++ palindromeRe_arrange.java
@@ -0,0 +1,11 @@
+
+public class palindromeRe_arrange {
+	public boolean palindromeRearranging(String inputString) {
+	    //get the letters. count the letters. If even then ok. if odd count the odd. if only one then ok.
+	    int letterCount = inputString.length()+1;//starting from 1
+	    if(letterCount % 2 != 0){
+	        
+	    }
+	}
+
+}
